2a2d81355ec152d744d0a79b68a44903a622dcb0
Craig Ondevilla <craiglo2@illinois.edu>
2023-11-13 17:27:41 -0600

    added more features to the website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className='header'>
+        <h1>Collageify!</h1>
+        <ul className="navigation">
+          {/* <a><li>Home</li></a>
+          <a><li>About</li></a> */}
+        </ul>
+      </div>
+      <div className='square-container'>
+        <h1>Collageify!</h1>
+        <div className='login-container'>
+          <label>Username</label>
+          <input type="text" placeholder="Enter Username" className="username" required/>
+          <label>Password</label>
+          <input type="text" placeholder="Enter Password" className="password" required/>
+          <button type="submit" className='login-button'>Login</button>
+        </div>
+      </div>
+      <div className='descriptions'>
+        <h2>Feature 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+           consequat.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Feature 2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+           consequat.
+        </p>
+        <h2>Feature 3</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+           consequat.
+        </p>
+      </div>
     </div>
   );
 }
